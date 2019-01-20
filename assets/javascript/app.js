5a165b450ff06800913b69e40798f228bc178039
Derek Ericson <derek.ericson@gmail.com>
2019-01-19 23:57:23 -0800

    added 60 sec game timer and tried to hide audio play button.  doesn't seem to be playing right now but should be easy fix.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,21 @@
 $(document).ready(function () {
-
-
+    $("#game-time").hide();
     $("#remaining-time").hide();
     $("#start").on('click', trivia.startGame);
     $(document).on('click', '.option', trivia.guessChecker);
-
+    $("#song").hide();
+    var song = document.getElementById("song");
+    song.play();
+    
 })
 
 var trivia = {
-
     correct: 0,
     incorrect: 0,
     unanswered: 0,
     currentSet: 0,
-    timer: 20,
+    timer: 0,
+    gtimer: 0,
     timerOn: false,
     timerId: '',
 
@@ -46,113 +48,94 @@ var trivia = {
     },
 
     startGame: function () {
-
+        trivia.gtimer = 15;
         trivia.currentSet = 0;
         trivia.correct = 0;
         trivia.incorrect = 0;
         trivia.unanswered = 0;
         clearInterval(trivia.timerId);
 
-
         $('#game').show();
-
         $('#results').html('');
-
         $('#timer').text(trivia.timer);
-
+        $('#gtimer').text(trivia.gtimer);
         $('#start').hide();
-
+        $('#game-time').show();
         $('#remaining-time').show();
-
         trivia.nextQuestion();
-
     },
 
     nextQuestion: function () {
-
-
         trivia.timer = 10;
         $('#timer').removeClass('last-seconds');
         $('#timer').text(trivia.timer);
 
-
         if (!trivia.timerOn) {
             trivia.timerId = setInterval(trivia.timerRunning, 1000);
+            trivia.timerOn = true;
         }
-
 
         var questionContent = Object.values(trivia.questions)[trivia.currentSet];
         $('#question').text(questionContent);
 
-
         var questionOptions = Object.values(trivia.options)[trivia.currentSet];
-
+        $('#options').html($(''));
         $.each(questionOptions, function (index, key) {
             $('#options').append($('<button class="option btn btn-default btn-lg">' + key + '</button>'));
         })
-
     },
 
     timerRunning: function () {
-
-        if (trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length) {
+        $("#gtimer").text(trivia.gtimer--);
+        if (trivia.gtimer == 0) {
+            trivia.showResults();
+        } else if (trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length) {
             $('#timer').text(trivia.timer);
             trivia.timer--;
-            if (trivia.timer === 4) {
+            if (trivia.timer === 3) {
                 $('#timer').addClass('last-seconds');
             }
         } else if (trivia.timer === -1) {
             trivia.unanswered++;
+            console.log("unanswered count " + trivia.unanswered);
             trivia.result = false;
             clearInterval(trivia.timerId);
             resultId = setTimeout(trivia.guessResult, 1000);
             $('#results').html('<h3>Pick faster! The answer was ' + Object.values(trivia.answers)[trivia.currentSet] + '</h3>');
         } else if (trivia.currentSet === Object.keys(trivia.questions).length) {
-
-
-            $('#results')
-                .html('<h3>Thanks for playing!</h3>' +
-                    '<p>Correct: ' + trivia.correct + '</p>' +
-                    '<p>Incorrect: ' + trivia.incorrect + '</p>' +
-                    '<p>Unaswered: ' + trivia.unanswered + '</p>' +
-                    '<p>Hopefully you learned some useless knowledge about Tiger today.</p>');
-
-
-            $('#game').hide();
-
-
-            $('#start').show();
+            trivia.showResults();
         }
+    },
 
+    showResults: function () {
+        clearInterval(trivia.timerId);
+        trivia.timerOn = false;
+
+        $('#results')
+            .html('<h3>Thanks for playing!</h3>' +
+                '<p>Correct: ' + trivia.correct + '</p>' +
+                '<p>Incorrect: ' + trivia.incorrect + '</p>' +
+                '<p>Unaswered: ' + trivia.unanswered + '</p>' +
+                '<p>Hopefully you learned some useless knowledge about Tiger today.</p>');
+
+        $('#game').hide();
+        $('#start').show();
     },
 
     guessChecker: function () {
-
-
-        var resultId;
-
-
         var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
-
-
         if ($(this).text() === currentAnswer) {
-
             $(this).addClass('btn-success').removeClass('btn-info');
-
             trivia.correct++;
-            clearInterval(trivia.timerId);
-            resultId = setTimeout(trivia.guessResult, 1000);
             $('#results').html('<h3>Correct!</h3>');
         } else {
-
             $(this).addClass('btn-danger').removeClass('btn-info');
-
             trivia.incorrect++;
-            clearInterval(trivia.timerId);
-            resultId = setTimeout(trivia.guessResult, 1000);
             $('#results').html('<h3>What were you thinking?! ' + currentAnswer + '</h3>');
         }
-
+        clearInterval(trivia.timerId);
+        trivia.timerOn = false;
+        setTimeout(trivia.guessResult, 1000);
     },
 
     guessResult: function () {
